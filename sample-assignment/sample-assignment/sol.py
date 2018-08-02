1f8ccd81d7f2de1da47d741438ecb76baa246a5b
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-02 11:41:33 +0530

    " -> 0 of 1 passed. pylint: 10.00/10 "

--- sample-assignment/sample-assignment/sol.py
+++ sample-assignment/sample-assignment/sol.py
@@ -1,12 +1,13 @@
-# Exercise: Assignment-1
-# Write a Python function, factorial(n), that takes in one number and
-# returns the factorial of given number.
-
-# This function takes in one number and returns one number.
+"""Exercise: Assignment-1
+Write a Python function, to add  given numbers.
+This function takes in one number and returns one number."""
 
 def main():
+    """
+    This function is the main function
+    """
     data = input()
-    print(int(data[0])+int(data[1]))
+    print(int(data[0]) + int(data[1]))
 
 if __name__ == "__main__":
     main()
