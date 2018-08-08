2aeba05c9e71c272027b143e656a6cc10892476d
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-08 14:56:52 +0530

    Activity 11 - M9

---
+++ m9/Functions and Objects Exercise-1/functions_and_objects_1.py
@@ -0,0 +1,17 @@
+#Exercise : Function and Objects Exercise-1
+#Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 4, 8, 9]
+
+
+def apply_to_each(L, f):
+    
+
+def main():
+    data = input()
+    data = data.split()
+    list1 = []
+    for j in l:
+        list1.append(int(j))
+    apply_to_each(list1, abs)
+
+if __name__ == "__main__":
+    main()
