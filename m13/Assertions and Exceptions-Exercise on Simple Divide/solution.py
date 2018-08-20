2bc58ab8edf7fa0347a698b790b5f2d20f977485
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-20 14:09:34 +0530

    M13-Activity 10

---
+++ m13/Assertions and Exceptions-Exercise on Simple Divide/solution.py
@@ -0,0 +1,27 @@
+'''define the simple_divide function here'''
+def simple_divide(item, denom):
+    '''fucntion'''
+    try:
+        return item/denom
+    except ZeroDivisionError:
+        return 0.0
+
+def fancy_divide(list_of_numbers, index):
+    '''fucntion'''
+    denom = list_of_numbers[index]
+    return [simple_divide(item, denom) for item in list_of_numbers]
+
+def main():
+    '''fucntion'''
+    data = input()
+    l_inp = data.split()
+    l1_inp = []
+    for j in l_inp:
+        l1_inp.append(float(j))
+    s_inp = input()
+    index = int(s_inp)
+    print(fancy_divide(l1_inp, index))
+
+
+if __name__ == "__main__":
+    main()
